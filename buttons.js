e6213b6d347f9266f9648b7b5a081f795663fe80
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-06-23 12:56:10 -0500

    added button variables for JS

--- buttons.js
+++ buttons.js
@@ -1,4 +1,8 @@
 let english = document.querySelector(".english");
 let spanish = document.querySelector(".spanish");
 let darkMode = document.querySelector(".dark-mode");
-let 
+let buttonCenter = document.querySelector(".button-center");
+let contactInfo = document.querySelector(".contact-info");
+let professionHeadshot = document.querySelector(".professional-headshot")
+let buttonLeft=document.querySelector(".button-left");
+let buttonRight=document.querySelector(".button-right");
