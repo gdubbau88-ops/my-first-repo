c03339e04c3101a9970b12e890a1a6d8705c134f
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-06-25 15:02:38 -0500

    task after talking with tanay

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # my-first-repo
-hi
-Turn off text decorations.
+finish translations
+replace headshot photo
+crop image gor MJFM logo
+home, about me, skills, projects, accesibility/settings
+contact me page
+style nav bar using div class nav
+swimming sea turtles
--- buttons.js
+++ buttons.js
@@ -4,5 +4,41 @@ let darkMode = document.querySelector(".dark-mode");
 let buttonCenter = document.querySelector(".button-center");
 let contactInfo = document.querySelector(".contact-info");
 let professionHeadshot = document.querySelector(".professional-headshot")
-let buttonLeft=document.querySelector(".button-left");
-let buttonRight=document.querySelector(".button-right");
+let buttonLeft = document.querySelector(".button-left");
+let buttonRight = document.querySelector(".button-right");
+let lang = "en";
+
+function setLang(l) {
+     localStorage.setItem("lang",l)
+    if (l === "en") {
+        buttonRight.innerHTML = "Choose Your Own Adventure Game";
+        buttonLeft.innerHTML = "Website for change";
+    } else if(l==="es"){
+        buttonRight.innerHTML = "Juego de elige tu propia aventura"
+        buttonLeft.innerHTML = "Sitio web para el cambio";
+    }else{
+
+    }
+
+}
+
+if (english === null) {
+    console.log("EN-null")
+} else {
+    english.addEventListener("click", function () {
+        setLang("en")
+    })
+};
+if (spanish === null) {
+    console.log("Es-null")
+} else {
+    spanish.addEventListener("click", function () {
+        setLang("es")
+    })
+};
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("loaded")
+    lang = localStorage.getItem("lang")
+    console.log(lang)
+    setLang(lang)
+})
