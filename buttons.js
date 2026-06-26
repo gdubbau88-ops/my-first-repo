5647d12d0fd8539f7295b3ad3a574d56c0df8390
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-06-26 16:54:05 -0500

    cant get home page turtle shell to translate

--- buttons.js
+++ buttons.js
@@ -7,15 +7,25 @@ let professionHeadshot = document.querySelector(".professional-headshot")
 let buttonLeft = document.querySelector(".button-left");
 let buttonRight = document.querySelector(".button-right");
 let lang = "en";
+let buttonProjects = document.querySelector(".button-projects");
+let buttonSkills = document.querySelector(".button-skills");
+let buttonSettings = document.querySelector(".button-settings");
+let buttonAboutMe = document.querySelector(".button-about-me");
+let buttonMyGithub = document.querySelector(".button-my-github");
+let buttonContactInfo = document.querySelector(".button-contact-info");
 
 function setLang(l) {
      localStorage.setItem("lang",l)
     if (l === "en") {
         buttonRight.innerHTML = "Choose Your Own Adventure Game";
         buttonLeft.innerHTML = "Website for change";
+        buttonSkills.innerHTML = "Skills";
+        buttonProjects.innerHTML="Projects";
     } else if(l==="es"){
         buttonRight.innerHTML = "Juego de elige tu propia aventura"
         buttonLeft.innerHTML = "Sitio web para el cambio";
+        buttonSkills.innerHTML = "Habilidades";
+        buttonProjects.innerHTML="Projectos";
     }else{
 
     }
